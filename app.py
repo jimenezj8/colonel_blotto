58184320c5fde2eab1eca9b4c6caf0e817c6c681
Jovier A. Jiménez <jthree8@gmail.com>
2022-11-20 12:08:13 -0800

    removing unnecessary error handling

--- app.py
+++ app.py
@@ -289,44 +289,40 @@ def update_home_tab(client, event, logger):
 
     database.get_user_signups(user_id)
 
-    try:
-        # views.publish is the method that your app uses to push a view to the Home tab
-        client.views_publish(
-            user_id=user_id,
-            view={
-                "type": "home",
-                "callback_id": "home_view",
-                "blocks": [
-                    {
-                        "type": "section",
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": "*Welcome to your _App's Home_* :tada:",
-                        },
+    # views.publish is the method that your app uses to push a view to the Home tab
+    client.views_publish(
+        user_id=user_id,
+        view={
+            "type": "home",
+            "callback_id": "home_view",
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "*Welcome to your _App's Home_* :tada:",
                     },
-                    {"type": "divider"},
-                    {
-                        "type": "section",
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app.",
-                        },
+                },
+                {"type": "divider"},
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app.",
                     },
-                    {
-                        "type": "actions",
-                        "elements": [
-                            {
-                                "type": "button",
-                                "text": {"type": "plain_text", "text": "Click me!"},
-                            }
-                        ],
-                    },
-                ],
-            },
-        )
-
-    except Exception as e:
-        logger.error(f"Error publishing home tab: {e}")
+                },
+                {
+                    "type": "actions",
+                    "elements": [
+                        {
+                            "type": "button",
+                            "text": {"type": "plain_text", "text": "Click me!"},
+                        }
+                    ],
+                },
+            ],
+        },
+    )
 
 
 @app.event("reaction_added")
