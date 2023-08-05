db47a0f39ef2fc3e3e8898b0b25ba4cbffcdb00d
Jovier A. Jiménez <jthree8@gmail.com>
2023-08-05 12:16:09 -0700

    forgotten strategy submission view definitions

--- views/new_submission.py
+++ views/new_submission.py
@@ -44,41 +44,45 @@ def update(
     round_soldiers: int,
     round_fields: int,
     round_rules: str,
-) -> dict[str, any]:
+) -> View:
     return View(
         type="modal",
-        callback_id="submit_strategy",
+        callback_id="strategy_submission_inputs_view",
         private_metadata=json.dumps({"game_id": game_id, "round_num": round_num}),
         title="Strategy submission",
         submit="Submit",
         close="Cancel",
-        blocks=[
-            blocks.HeaderBlock(text="General rules"),
-            blocks.SectionBlock(
-                text=blocks.MarkdownTextObject(text=messages.general_rules)
-            ),
-            blocks.HeaderBlock(text="Round rules"),
-            blocks.SectionBlock(
-                text=blocks.MarkdownTextObject(text=round_rules),
-            ),
-            blocks.DividerBlock(),
-            blocks.SectionBlock(
-                text=(
-                    f"You have {round_soldiers} soldiers "
-                    f"to distribute amongst {round_fields} fields. "
-                    "Good luck, Major!"
+        blocks=(
+            [
+                blocks.HeaderBlock(text="General rules"),
+                blocks.SectionBlock(
+                    text=blocks.MarkdownTextObject(text=messages.general_rules)
                 ),
-            ),
-        ]
-        + [
-            blocks.InputBlock(
-                label=f"Field {field_num + 1}",
-                element=blocks.PlainTextInputElement(
-                    action_id=f"field-{field_num + 1}",
-                    placeholder=blocks.PlainTextObject("Enter a number of soldiers"),
+                blocks.HeaderBlock(text="Round rules"),
+                blocks.SectionBlock(
+                    text=blocks.MarkdownTextObject(text=round_rules),
                 ),
-                block_id=f"field-{field_num + 1}",
-            )
-            for field_num in range(round.fields)
-        ],
+                blocks.DividerBlock(),
+                blocks.SectionBlock(
+                    text=(
+                        f"You have {round_soldiers} soldiers "
+                        f"to distribute amongst {round_fields} fields. "
+                        "Good luck, Major!"
+                    ),
+                ),
+            ]
+            + [
+                blocks.InputBlock(
+                    label=f"Field {field_num + 1}",
+                    element=blocks.PlainTextInputElement(
+                        action_id=f"field-{field_num + 1}",
+                        placeholder=blocks.PlainTextObject(
+                            text="Enter a number of soldiers"
+                        ),
+                    ),
+                    block_id=f"field-{field_num + 1}",
+                )
+                for field_num in range(round_fields)
+            ]
+        ),
     )
